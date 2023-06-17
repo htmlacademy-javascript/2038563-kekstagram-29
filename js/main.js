33126d9ce6993e5ae7b69317a69ff26c4175810e
Елена Борисова <len-bo@mail.ru>
2023-06-18 02:34:12 +0300

    without homeworks

---
+++ js/main.js
@@ -0,0 +1,19 @@
+{
+  id: 1,
+  url: 'photos/5.jpg',
+  description: 'описание фотографии',
+  likes:  7,
+  comments: []
+}
+
+
+const PHOTOS = 25;
+
+const getOnePhoto = () => ({})
+
+const getPhotos = (count) => {
+
+};
+
+
+console.log(getPhotos(PHOTOS));
