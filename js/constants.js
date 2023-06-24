b1a7d667c419370cfbbf008c4780b4310722c01f
Елена Борисова <len-bo@mail.ru>
2023-06-24 17:47:53 +0300

    module

---
+++ js/constants.js
@@ -0,0 +1,49 @@
+
+const PHOTOS = 25;
+
+const MIN_LIKES = 0;
+const MAX_LIKES = 999;
+
+const DESCRIPTIONS = [
+  'утро',
+  'котик',
+  'масик',
+  'пупсик',
+  'закат в Париже',
+];
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 12;
+
+const MESSAGE= [
+  'прекрасно',
+  'неплохо',
+  'полное г***о',
+  'что за шляпа?'
+];
+
+const COMMENTS = 500;
+
+const MIN_AVATAR = 1;
+const MAX_AVATAR = 100;
+
+const NAMES = [
+  'Виктория',
+  'Иван',
+  'Коля',
+  'Настя',
+  'Маруся'
+];
+
+export {
+PHOTOS,
+MIN_LIKES,
+MAX_LIKES,
+DESCRIPTIONS,
+MIN_COMMENTS,
+MAX_COMMENTS,
+MESSAGE,
+COMMENTS,
+MIN_AVATAR,
+MAX_AVATAR,
+NAMES,
+};
--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-
-
-{
-  id: 1,
-  url: 'photos/5.jpg',
-  description: 'описание фотографии',
-  likes:  7,
-  comments: []
-}
-
-
-const PHOTOS = 25;
-
-const getOnePhoto = () => ({})
-
-const getPhotos = (count) => {
-
-};
-
+import {getPhotos} from './data.js';
+import {PHOTOS} from './constants.js';
 
 console.log(getPhotos(PHOTOS));
+
